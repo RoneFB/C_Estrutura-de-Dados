a741ba3c3451e6b32504e02855928bb9ba28bd8d
Rone Felipe Bento <rone.bento@alltec.local>
2019-09-24 14:30:45 -0300

    Correção Pilha, Cabeça

--- Alocacao Dinamica/Pilha/Pilha.c
+++ Alocacao Dinamica/Pilha/Pilha.c
@@ -9,55 +9,52 @@ typedef struct celula{
 celula * cria_Pilha(){
     celula *p;
     p = malloc(sizeof(celula));
-    p = NULL;
+    p->prox = NULL;
+    p->valor = 0;
     return p;
 }
-void empilha(int valor, celula **P){
+void empilha(int valor, celula *P){
     celula * nova = malloc(sizeof(celula));
     nova->valor = valor;
-    nova->prox = (*P);//aponta o prox para o topo da pilha
-    *P = nova;// atualiza o topo da pilha
+    nova->prox = P->prox; //aponta o prox para o topo da pilha
+    P->prox = nova;// atualiza o topo da pilha
 }
-void desempilhar(celula **P){
-    if((*P)->prox!= NULL){
-        celula *lixo;
-        lixo = (*P);
-        *P = lixo->prox;
+void desempilhar(celula *P){
+    if(P->prox!= NULL){
+        celula *lixo = P->prox;
+        P->prox = lixo->prox;
         free(lixo);
-    }else{
-        *P = NULL;
-        printf("Pilha Vazia");
     }
 }
  void imprimir_pilha(celula *P) {
-    for (celula *aux = P; aux != NULL; aux = aux->prox)
+    for (celula *aux = P->prox; aux != NULL; aux = aux->prox)
         printf("%d\n", aux->valor);
  }
 
 int main(){
     celula *p = cria_Pilha();
     printf("\Empilhando\n");
-    empilha(11,&p);
-    empilha(8,&p);
-    empilha(4,&p);
-    empilha(3,&p);
-    empilha(2,&p);
+    empilha(11,p);
+    empilha(8,p);
+    empilha(4,p);
+    empilha(3,p);
+    empilha(2,p);
     imprimir_pilha(p);
 
     printf("\nDesempilhando\n");
-    desempilhar(&p);
+    desempilhar(p);
     imprimir_pilha(p);
     printf("\nDesempilhando\n");
-    desempilhar(&p);
+    desempilhar(p);
     imprimir_pilha(p);
     printf("\nDesempilhando\n");
-    desempilhar(&p);
+    desempilhar(p);
     imprimir_pilha(p);
     printf("\nDesempilhando\n");
-    desempilhar(&p);
+    desempilhar(p);
     imprimir_pilha(p);
     printf("\nDesempilhando\n");
-    desempilhar(&p);
+    desempilhar(p);
     imprimir_pilha(p);
 
 }
--- Alocacao Dinamica/Pilha/PilhaSomaIterativo.c
+++ Alocacao Dinamica/Pilha/PilhaSomaIterativo.c
@@ -9,58 +9,50 @@ typedef struct celula{
 celula * cria_Pilha(){
     celula *p;
     p = malloc(sizeof(celula));
-    p = NULL;
+    p->prox = NULL;
+    p->valor = 0;
     return p;
 }
-void empilha(int valor, celula **P){
+void empilha(int valor, celula *P){
     celula * nova = malloc(sizeof(celula));
     nova->valor = valor;
-    nova->prox = (*P);//aponta o prox para o topo da pilha
-    *P = nova;// atualiza o topo da pilha
+    nova->prox = P->prox; //aponta o prox para o topo da pilha
+    P->prox = nova;// atualiza o topo da pilha
 }
-int desempilhar(celula **P){
+int desempilhar(celula *P){
     int valor;
-    celula *lixo;
-    lixo = (*P);
-    if((*P)->prox!= NULL){
-        *P = lixo->prox;
+    if(P->prox!= NULL){
+        celula *lixo = P->prox;
+        P->prox = lixo->prox;
         valor = lixo->valor;
         free(lixo);
-        return valor;
-    }else{
-        valor = lixo->valor;
-        (*P) = NULL;
-        return valor;
     }
-    return NULL;
+    return valor;
 }
-
 void imprimir_pilha(celula *P) {
-    for (celula *aux = P; aux != NULL; aux = aux->prox)
+    for (celula *aux = P->prox; aux != NULL; aux = aux->prox)
         printf("%d\n", aux->valor);
 }
 
-int somaPilha(celula **p) {
+int somaPilha(celula *p) {
     int soma = 0;
-    while ((*p)->prox != NULL) {
-        if ((*p)->valor > 0)
-            soma += desempilhar(&(*p));
-        else
-            desempilhar(&(*p));
-    }soma += desempilhar(&(*p));
+    while (p->prox != NULL) {
+        if (p->prox->valor > 0)
+            soma += desempilhar(p);
+    }
     return soma;
 }
 
 int main(){
     celula * p = cria_Pilha();
 
-    empilha(2,&p);
-    empilha(4,&p);
-    empilha(8,&p);
-    empilha(16,&p);
-    empilha(32,&p);
+    empilha(2,p);
+    empilha(4,p);
+    empilha(8,p);
+    empilha(16,p);
+    empilha(32,p);
 
     imprimir_pilha(p);
-    printf("Soma = %d", somaPilha(&p));
-    imprimir_pilha(p);
+    printf("Soma = %d", somaPilha(p));
+
 }
